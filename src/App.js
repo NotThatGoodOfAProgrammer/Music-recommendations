901af79d20584bff156dc944ea7a4225b6c6ae83
Konrad <karmatys888@gmail.com>
2023-08-25 11:39:59 +0200

    improved mobile design
    changed from viewport units to percentages because on mobile it doesn't include whole screen + changed height to max-height to get rid of redundant space

--- src/App.js
+++ src/App.js
@@ -299,7 +299,7 @@ function Placeholder() {
 
 function App() {
   const CLIENT_ID = "eae286ae2c30452f876d62116733da2a";
-  const REDIRECT_URI = "https://NotThatGoodOfAProgrammer.github.io/Music-recommendations";
+  const REDIRECT_URI = "https://notthatgoodofaprogrammer.github.io/Music-recommendations";
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
   const perPage = 20;
